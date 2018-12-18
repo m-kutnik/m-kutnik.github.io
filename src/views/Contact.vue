f4b91435d4beae22cf90725f39883f3effe2a186
Mike <gimp2312@gmail.com>
2018-12-18 19:42:09 +0100

    create Contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -5,13 +5,38 @@
       ... or take me with you! 🙋‍♂️
     </h2>
     <p>You can find me here:</p>
-    <div class="social"></div>
+    <div class="social">
+      <a
+        href="https://www.linkedin.com/in/micha%C5%82-kutnik-065538131/"
+        target="_blank"
+        rel="noopener noreferrer"
+        ><i class="fab fa-linkedin"></i
+      ></a>
+      <a
+        href="https://github.com/m-kutnik"
+        target="_blank"
+        rel="noopener noreferrer"
+        ><i class="fab fa-github"></i
+      ></a>
+      <a href="http://m.me/m2.kutnik" target="_blank" rel="noopener noreferrer"
+        ><i class="fab fa-facebook-messenger"></i
+      ></a>
+    </div>
   </section>
 </template>
 <style lang="scss" scoped>
 p {
   line-height: 2.5em;
   font-size: 16px;
-  padding: 0 0 40px 0;
+}
+.social {
+  font-size: 46px;
+  a {
+    padding: 0 15px;
+
+    &:hover {
+      color: #949494;
+    }
+  }
 }
 </style>
