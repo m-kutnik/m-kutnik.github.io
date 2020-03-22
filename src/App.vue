49ab76174d49666cb024c48f7719082b430ed411
Mike <gimp2312@gmail.com>
2020-03-22 12:28:45 +0100

    Add Muli font, add some global styles

--- src/App.vue
+++ src/App.vue
@@ -18,10 +18,26 @@ export default {
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Muli:200i,200,500i,700i&display=swap&subset=latin-ext');
+
 html {
   font-family: 'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+:root {
+  --link: var(--secondary);
+}
+
+.h1,
+.h2,
+.h3,
+.h4,
+.h5,
+.h6,
+.subtitle {
+  font-family: 'Muli', sans-serif;
 }
 
 /* Transitions */
