a32fe54e9ad858c94fec61e22f5fe332f395a2ac
Mike Kutnik <gimp2312@gmail.com>
2020-03-29 01:34:43 +0100

    Add scrollBehavior to vue router

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,18 @@ Vue.use(VueRouter)
 const router = new VueRouter({
   routes,
   mode: 'history',
+  scrollBehavior(to) {
+    if (to.hash) {
+      const element = document.querySelector(to.hash)
+      if (element) {
+        window.scrollTo({
+          top: element.offsetTop,
+          behavior: 'smooth',
+        })
+      }
+    }
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  },
 })
 
 export default router
