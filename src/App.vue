7179051a26ed5573817e5a9bfc7127524f133489
Mike Kutnik <gimp2312@gmail.com>
2020-03-29 23:38:10 +0200

    Move font import to index.html

--- src/App.vue
+++ src/App.vue
@@ -20,8 +20,6 @@ export default {
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Muli:200i,200,500i,700i&display=swap&subset=latin-ext');
-
 html {
   font-family: 'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
