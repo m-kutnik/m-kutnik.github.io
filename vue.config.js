fc8a100f3216ab26d69cdfde9461867f7be25e0b
Mike Kutnik <gimp2312@gmail.com>
2020-03-29 01:33:34 +0100

    Fix PrerenderSPAPlugin running on serve script

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,12 @@ const path = require('path') // eslint-disable-line
 const PrerenderSPAPlugin = require('prerender-spa-plugin')
 
 module.exports = {
-  configureWebpack: {
-    plugins: [
-      new PrerenderSPAPlugin({
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugin('prerender').use(PrerenderSPAPlugin, {
         staticDir: path.join(__dirname, 'dist'),
         routes: ['/', '/about', '/projects', '/contact'],
-      }),
-    ],
+      })
+    }
   },
 }
