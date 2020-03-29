9eb34af429dff3fbe1d2df082d6be37507832725
Mike Kutnik <gimp2312@gmail.com>
2020-03-29 23:43:21 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,24 @@
 const path = require('path') // eslint-disable-line
 const PrerenderSPAPlugin = require('prerender-spa-plugin')
 
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
+
 module.exports = {
-  configureWebpack: config => {
+  publicPath: '',
+  devServer: {
+    disableHostCheck: true,
+  },
+  chainWebpack: config => {
     if (process.env.NODE_ENV === 'production') {
-      config.plugin('prerender').use(PrerenderSPAPlugin, {
-        staticDir: path.join(__dirname, 'dist'),
-        routes: ['/', '/about', '/projects', '/contact'],
-      })
+      // config.plugin('prerender').use(PrerenderSPAPlugin, [
+      //   {
+      //     staticDir: path.join(__dirname, 'dist'),
+      //     routes: ['/'],
+      //     renderer: new Renderer({
+      //       args: ['--disable-gpu'],
+      //     }),
+      //   },
+      // ])
     }
   },
 }
