eb5c93ac43b634d4408005ea4dba3b1ac09deb6f
Mike Kutnik <gimp2312@gmail.com>
2020-03-29 01:47:08 +0100

    Change scrollBehavior

--- src/router/index.js
+++ src/router/index.js
@@ -7,17 +7,8 @@ Vue.use(VueRouter)
 const router = new VueRouter({
   routes,
   mode: 'history',
-  scrollBehavior(to) {
-    if (to.hash) {
-      const element = document.querySelector(to.hash)
-      if (element) {
-        window.scrollTo({
-          top: element.offsetTop,
-          behavior: 'smooth',
-        })
-      }
-    }
-    window.scrollTo({ top: 0, behavior: 'smooth' })
+  scrollBehavior() {
+    return { x: 0, y: 0 }
   },
 })
 
