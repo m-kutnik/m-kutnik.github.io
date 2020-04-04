00346a07ee3af0870c449f26816a66659ee51f0b
Mike Kutnik <gimp2312@gmail.com>
2020-04-04 03:30:30 +0200

    Fix lottie path in 404 view

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,6 +1,6 @@
 <template functional>
   <article class="container text-centered">
     <h1 class="h2">Page not found</h1>
-    <lottie-animation path="/lotties/404.json" />
+    <lottie-animation path="lotties/404.json" />
   </article>
 </template>
