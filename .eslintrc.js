17b6b8e193bf5cf6d3d4b8e03c6b7e30bae99c4e
Mike Kutnik <gimp2312@gmail.com>
2020-04-04 03:28:54 +0200

    Update linters configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,13 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unused-expressions': [
+      'error',
+      {
+        allowShortCircuit: true,
+        allowTernary: true,
+      },
+    ],
   },
   settings: {
     'import/resolver': {
--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,4 +1,30 @@
 module.exports = {
-  root: true,
-  extends: 'stylelint-config-standard',
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-idiomatic-order',
+    'stylelint-config-prettier',
+    'stylelint-config-sass-guidelines',
+  ],
+  plugins: ['stylelint-scss'],
+  rules: {
+    'order/properties-alphabetical-order': null,
+    'string-no-newline': null,
+    'selector-class-pattern': /^[a-zA-Z-]*(-(enter|leave)(-(active|to))?)?$/,
+    'selector-id-pattern': /^[a-zA-Z-]*$/,
+    'selector-max-universal': 1,
+    'selector-max-type': [0, { ignore: ['child', 'descendant', 'compounded'] }],
+    // ===
+    // SCSS
+    // ===
+    'scss/dollar-variable-colon-space-after': 'always',
+    'scss/dollar-variable-colon-space-before': 'never',
+    'scss/dollar-variable-no-missing-interpolation': true,
+    'scss/dollar-variable-pattern': /^[a-z-]+$/,
+    'scss/double-slash-comment-whitespace-inside': 'always',
+    'scss/operator-no-newline-before': true,
+    'scss/operator-no-unspaced': true,
+    'scss/selector-no-redundant-nesting-selector': true,
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+  },
 }
