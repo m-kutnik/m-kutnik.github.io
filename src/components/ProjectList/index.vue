90f45c471139840894fd5e6038b88d7bda436dab
Mike Kutnik <gimp2312@gmail.com>
2020-04-04 03:33:42 +0200

    Update ProjectList

--- src/components/ProjectList/index.vue
+++ src/components/ProjectList/index.vue
@@ -1,15 +1,29 @@
 <template>
-  <section>
-    <project-list-header :tags="tags" />
+  <section class="section">
+    <project-list-header />
     <div class="project-container">
       <transition-group class="list" name="list" tag="ul">
         <li
           v-for="item in filteredItems"
           :key="item.name"
-          class="project"
+          class="project-wrapper"
           :class="item.size"
+          @mouseenter="item.hovered = true"
+          @mouseleave="item.hovered = false"
         >
-          <h2>{{ item.name }}</h2>
+          {{ item.backgroundStyle }}
+          <div
+            class="project-background"
+            :style="`background: ${item.background}`"
+          />
+          <div class="project-summary">
+            <h2>{{ item.name }}</h2>
+          </div>
+          <app-image-wrapper
+            class="project-image"
+            :image="item.image"
+            :alt="item.alt"
+          />
         </li>
       </transition-group>
     </div>
@@ -17,14 +31,16 @@
 </template>
 
 <script>
-import { computed } from '@vue/composition-api'
+import { ref, computed } from '@vue/composition-api'
 import { useQuery } from '@/use/router'
+import AppImageWrapper from '@/components/AppImageWrapper'
 import ProjectListHeader from './ProjectListHeader'
 
 export default {
   name: 'ProjectList',
   components: {
     ProjectListHeader,
+    AppImageWrapper,
   },
   props: {
     items: {
@@ -33,33 +49,77 @@ export default {
     },
   },
   setup(props) {
+    const isHovered = ref(false)
     const { search } = useQuery()
 
-    const tagsArray = props.items.flatMap(item => [
-      ...item.tags,
-      ...item.technologies,
-    ])
-    const tags = new Set(tagsArray)
+    const filteredItems = computed(() => {
+      if (!search.value) {
+        return props.items
+      }
 
-    const filteredItems = computed(() =>
-      props.items.filter(item => {
-        return item.name.toLowerCase().includes(search.value.toLowerCase())
-      }),
-    )
+      return props.items.filter(item => {
+        const values = Object.values(item)
+          .flat()
+          .filter(value => typeof value === 'string')
+          .map(value => value.toLowerCase())
 
-    return { filteredItems, tags }
+        return values.some(element =>
+          element.includes(search.value.toLowerCase()),
+        )
+      })
+    })
+
+    return { isHovered, filteredItems }
   },
 }
 </script>
 
 <style lang="scss" scoped>
-section {
+.section {
   margin: 6rem 0;
 }
 
-.project {
+.project-background {
+  position: absolute;
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+  transform: scale(0.9, 0.9);
+  transition: all 0.3s var(--transition-function);
+}
+
+.project-image {
+  position: relative;
+  z-index: 2;
+  width: 100%;
+  height: auto;
+  opacity: 1;
+  transform: translateY(0);
+  transition: opacity 0.3s var(--transition-function),
+    transform 0.3s var(--transition-function);
+}
+
+.project-summary {
+  position: absolute;
+  z-index: 2;
+  width: calc(100% - 40px);
+  height: calc(100% - 40px);
+  padding: 10px;
+  margin: 20px;
+  background: #fff;
+  opacity: 0;
+  transition: opacity 0.3s var(--transition-function),
+    transform 0.3s var(--transition-function);
+
+  .is-dark & {
+    background: #272727;
+  }
+}
+
+.project-wrapper {
+  position: relative;
+  overflow: hidden;
   background: hsla(0, 0%, 50%, 0.15);
-  padding: 20px;
   grid-column: span 1;
 
   &.big {
@@ -69,18 +129,32 @@ section {
       grid-column: span 1;
     }
   }
+
+  &:hover .project-background {
+    transform: scale(1, 1);
+  }
+
+  &:hover .project-image {
+    opacity: 0;
+    transform: translateY(200px);
+  }
+
+  &:hover .project-summary {
+    opacity: 1;
+    transform: scale(1, 1);
+  }
 }
 
 .list {
+  z-index: 1;
   display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 1rem;
-  grid-auto-flow: dense;
   justify-content: center;
-  list-style-type: none;
   padding: 0;
   backface-visibility: hidden;
-  z-index: 1;
+  grid-auto-flow: dense;
+  grid-gap: 1rem;
+  grid-template-columns: 1fr 1fr;
+  list-style-type: none;
 
   @media screen and (max-width: 48rem) {
     grid-template-columns: 1fr;
@@ -98,8 +172,8 @@ section {
 
   /* disappearing */
   &-leave-active {
-    transition: opacity 300ms ease-in;
     z-index: 0;
+    transition: opacity 300ms ease-in;
   }
 
   /* appear at / disappear to */
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -2,6 +2,7 @@
   <article class="section">
     <div class="container is-thin">
       <h1 class="h3">My projects 🦥</h1>
+      <p class="size-5">Search by name, technology, anything you want...</p>
     </div>
     <div class="container">
       <project-list :items="projects" />
@@ -10,7 +11,15 @@
 </template>
 
 <script>
+/* eslint-disable global-require */
 import ProjectList from '@/components/ProjectList'
+
+const NODE_TAGS = ['node', 'nodejs', 'node.js']
+const JAVASCRIPT_TAGS = ['js', 'javascript', 'es6']
+const HTML_TAGS = ['html5', 'html']
+const CSS_TAGS = ['scss', 'sass', 'css']
+const VUE_TAGS = ['vue', 'vuejs', 'vue.js']
+const GCP_TAGS = ['gcp', 'google', 'cloud', 'platform']
 
 export default {
   name: 'Projects',
@@ -22,45 +31,74 @@ export default {
       {
         name: 'Gitlab Contribution Sync',
         descriptions: 'Test1',
-        size: 'big',
         tags: ['Utility'],
-        technologies: ['Node', 'JS'],
+        technologies: [...NODE_TAGS, ...JAVASCRIPT_TAGS],
+        background:
+          'linear-gradient(135deg, rgba(252,109,38,1) 0%, rgba(131,58,180,1) 100%)',
+        hovered: false,
+        image: require('@/assets/images/gitlab-sync.png'),
       },
       {
         name: 'Yata - Twitch Autoclicker',
         descriptions: 'Test1',
         tags: ['Utility'],
-        technologies: ['JS'],
+        technologies: [...JAVASCRIPT_TAGS],
+        image: require('@/assets/images/gitlab-sync.png'),
       },
       {
         name: 'Dwarf.css',
         descriptions: 'Test1',
         tags: ['Utility'],
-        technologies: ['SCSS', 'HTML5'],
+        technologies: [...HTML_TAGS, ...CSS_TAGS],
+        image: require('@/assets/images/gitlab-sync.png'),
       },
       {
         name: 'Project Makaron.js',
         descriptions: 'Test1',
         tags: ['Utility'],
-        technologies: ['Node', 'JS'],
+        technologies: [...NODE_TAGS, ...JAVASCRIPT_TAGS],
+        image: require('@/assets/images/gitlab-sync.png'),
       },
       {
         name: 'Spotify Scoreboard',
         descriptions: 'Test1',
         tags: ['App'],
-        technologies: ['Vue', 'HTML5', 'JS', 'Lodash', 'OAuth'],
+        technologies: [
+          ...VUE_TAGS,
+          ...NODE_TAGS,
+          ...JAVASCRIPT_TAGS,
+          'lodash',
+          'oauth',
+        ],
+        image: require('@/assets/images/gitlab-sync.png'),
       },
       {
         name: 'Bulma Themes',
         descriptions: 'Test1',
         tags: ['App'],
-        technologies: ['Vue', 'HTML5', 'JS', 'GCP'],
+        technologies: [
+          ...VUE_TAGS,
+          ...NODE_TAGS,
+          ...JAVASCRIPT_TAGS,
+          ...HTML_TAGS,
+          ...GCP_TAGS,
+        ],
+        image: require('@/assets/images/gitlab-sync.png'),
       },
       {
         name: 'Project NORA',
         descriptions: 'Test1',
         tags: ['App'],
-        technologies: ['Vue', 'HTML5', 'JS', 'GCP', 'WebSocket', 'IRC'],
+        technologies: [
+          ...VUE_TAGS,
+          ...HTML_TAGS,
+          ...JAVASCRIPT_TAGS,
+          ...GCP_TAGS,
+          'websocket',
+          'ws',
+          'irc',
+        ],
+        image: require('@/assets/images/gitlab-sync.png'),
       },
     ]
 
@@ -68,5 +106,3 @@ export default {
   },
 }
 </script>
-
-<style></style>
