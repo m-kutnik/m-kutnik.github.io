0f82c125b485ac324e172e6bb8cfc9e09af6dad0
Mike Kutnik <gimp2312@gmail.com>
2020-04-04 03:31:11 +0200

    Remove tags from router composition function

--- src/use/router.js
+++ src/use/router.js
@@ -6,10 +6,9 @@ export const useQuery = () => {
   const vm = getVM()
   const query = computed(() => vm.$route.query)
   const search = computed(() => query.value.search || '')
-  const tags = computed(() => query.value.tags)
 
   const update = params =>
     router.replace({ query: { ...vm.$route.query, ...params } }).catch(() => {})
 
-  return { query, search, tags, update }
+  return { query, search, update }
 }
