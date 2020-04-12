f7c134fb3c204503077939bbd2050a642005022e
m-kutnik <m-kutnik@users.noreply.github.com>
2020-04-12 20:57:23 +0000

    deploy: 2cdbabb798cb163673603d1a92e2a6fc756d29fb

--- js/chunk-2d0e95df.69a92f6d.js
+++ js/chunk-2d0e95df.69a92f6d.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e95df"],{"8cdb":function(t,n,a){"use strict";a.r(n);var e=function(t,n){var a=n._c;return a("article",{staticClass:"container text-centered"},[a("h1",{staticClass:"h2"},[n._v("Page not found")]),a("lottie-animation",{attrs:{path:"lotties/404.json"}})],1)},s=[],c=a("2877"),o={},i=Object(c["a"])(o,e,s,!0,null,null,null);n["default"]=i.exports}}]);
-//# sourceMappingURL=chunk-2d0e95df.b9211cf4.js.map
+//# sourceMappingURL=chunk-2d0e95df.69a92f6d.js.map
